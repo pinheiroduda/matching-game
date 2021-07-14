0ddd86e02a32d21a4177459250bff38b20eed583
Eduarda <eduardapinheiro1302@gmail.com>
2021-07-14 14:11:15 -0300

    feat: max-width to body and CardGame style

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ import './src/styles/elements/base.css'
 import BoardGame from './src/Objects/BoardGame'
 
 const $root = document.querySelector('#root')
-const $htmlBoardGame = BoardGame(3)
+const $htmlBoardGame = BoardGame(6)
 
 $root.insertAdjacentHTML('beforeend', $htmlBoardGame)
