7d322bce44973c8998b3461a0cf3ff877de1061e
Eduarda <eduardapinheiro1302@gmail.com>
2021-07-13 22:55:42 -0300

    feat: card, styles and reset CSS

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,6 +1,9 @@
+import './style.css'
+
 function CardGame() {
   return `
-    <article>
+    <article class="card-game">
+      <img src="images/alura-pixel.png" alt="logo da alura">
     </article>
   `
 }
